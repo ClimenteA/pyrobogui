0066d90992ecae200adfbe812ac7466acf583826
Climente Alin <aclimente@ymail.com>
2018-11-15 12:09:09 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,15 +4,22 @@ from setuptools import setup, find_packages
 #cd dist 
 #twine upload *
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup (
 	name="pyrobogui",
-	version="0.0.1",
-	description="Wrapper around pyautogui - plus some new functions ",
+	version="0.0.2",
+	description="Wrapper around pyautogui for automating mouse and keyboard - plus some new functions",
 	url="https://github.com/ClimenteA/pyrobogui",
 	author="Climente Alin",
 	author_email="climente.alin@gmail.com",
 	license='MIT',
 	py_modules=["pyrobogui"],
 	packages=find_packages(),
+	long_description=long_description,
+    long_description_content_type="text/markdown",
 	package_dir={"":"src"}
 )
